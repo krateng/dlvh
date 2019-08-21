aab4c615684bfa9dc874a82e8f1eed623d8d8664
Krateng <git.noreply@krateng.ch>
2019-08-21 14:57:33 +0200

    Initial commit

---
+++ borgsize.py
@@ -0,0 +1,24 @@
+# Lists sizes of all backups in a borg repository
+
+import sys
+import os
+import re
+import tabulate
+
+repo = sys.argv[1]
+
+archivelist = os.popen("borg list " + repo).read()
+
+archives = archivelist.split("\n")[:-1]
+archives = [a.split() for a in archives]
+archivenames = [a[0] for a in archives]
+
+sizes = []
+for a in archivenames:
+	info = os.popen("borg info " + repo + "::" + a).read()
+	info = info.split("\n")
+	info = [line for line in info if line.startswith("This archive:")][0]
+	_, origsize, compressedsize, dedupsize = re.split(r'\s{2,}', info)
+	sizes.append([a,origsize,compressedsize,dedupsize])
+	
+print(tabulate.tabulate(sizes,headers=["Archive","Original Size","Compressed Size","Deduplicated Size"]))
